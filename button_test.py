9b70290697cc34060b517b60eea3f2749fd77854
Ozero <mehes002@umn.edu>
2024-11-06 13:04:18 -0600

    debug

--- button_test.py
+++ button_test.py
@@ -1,7 +1,14 @@
-
+import subprocess
 from gpiozero import Button
+import time
+subprocess.Popen(['sudo', 'rfcomm', 'connect', '0', '24:54:89:AE:0A:51'])
+
+print('c')
+time.sleep(5)
 
 button = Button(2)
 
 button.wait_for_press()
 print('You pushed me')
+
+subprocess.run(['python', 'thermal-print.py', 'image.png', '>', '/dev/rfcomm0'])
